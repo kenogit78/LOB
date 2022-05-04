1176e1a08cb7b2ae895fb0761a9a3e75e321dd22
Odizee <odizeeplatform@gmail.com>
2022-05-04 04:40:25 -1100

    bottom nav fix

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -29,6 +29,10 @@ const Home = () => {
 
   const dispatch = useDispatch();
 
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
   const { user, post } = useSelector((state) => state.auth);
 
   const userId = user?.data.user._id;
